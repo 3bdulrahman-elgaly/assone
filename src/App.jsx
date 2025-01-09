6dfe756678c6a049037d54983036f46fd9e2f3c8
3bdulrahman-elgaly <algalycom30@gmail.com>
2025-01-09 15:25:03 +0200

    f

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,12 @@ import Home from "./components/Home/Home";
 import About from "./components/About/About";
 import Portfolio from "./components/Portfolio/Portfolio";
 import Contact from "./components/Contact/Contact";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { RouterProvider, createBrowserRouter, createHashRouter } from "react-router-dom";
 import Layout from "./components/Layout/Layout";
 import Erorr from "./components/Erorr/Erorr";
 
 export default function App() {
-  const router = createBrowserRouter([
+  const router = createHashRouter([
     {   path: "",     element: <Layout />, errorElement: <Erorr />,    children: [
         { index:true,   element: <Home />  },
         {     path: "/home", element: <Home />},
